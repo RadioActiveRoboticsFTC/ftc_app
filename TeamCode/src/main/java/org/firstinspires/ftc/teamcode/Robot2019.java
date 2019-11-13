6775ada1f1fef486cb21708c6157e23d8611c011
Paul Marganian <pmargani@nrao.edu>
2019-11-12 19:01:39 -0500

    testing out git.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2019.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2019.java
@@ -55,6 +55,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
  * Servo channel:  Servo to open left claw:  "left_hand"
  * Servo channel:  Servo to open right claw: "right_hand"
  */
+
+// this is a test comment
 public class Robot2019
 {
     /* Public OpMode members. */
