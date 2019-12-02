b16eb77769c07d6dff7c32988ac96906b58b2e66
Paul <RadioactiveRoboticsFTC@gmail.com>
2019-12-02 06:42:29 -0500

    Autonomous1 parks under sky bridge
    is example of simplest way to create an ops mode
    from BaseAutonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+// Directions:
+// Place robot against wall, facing the sky bridge, either side.  Have
+// the front of the robot almost under the sky bridge. See Diagram ???
+
+// Points:
+// 5 points for parking the robot directly under the sky bridge
+
+// This is an example of creating an autonomous mode based off of
+// BaseAutonomous (we 'extend' it)
+
+@Autonomous(name="Autonomous1")
+public class Autonomous1 extends BaseAutonomous {
+
+    // The BaseAutonomous class will have it's runOpsMode function called.  It will set up
+    // basic stuff for us, but then it calls it's own runAutoOpMode, which does nothing.
+    // We 'overrid' runAutoOpMode to do *something*
+    @Override
+    public void runAutoOpMode() {
+
+        // driving straight this distance parks the robot directly under
+        // the sky bridge
+        driveStraight(0.5, 9.0, 15.0);
+    }
+
+}
