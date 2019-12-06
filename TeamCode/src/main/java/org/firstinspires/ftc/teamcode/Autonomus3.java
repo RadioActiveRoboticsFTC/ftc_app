0ce3540e57de27b44dd80be36639f76db050c1bd
Paul Marganian <pmargani@nrao.edu>
2019-12-05 20:08:32 -0500

    Autonomus3 parks uor robot on the farther side of the blue skybridge!!:)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomus3.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+//import BaseAutonomous;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+// Directions:
+
+
+// Points:
+// 5 points for parking the robot directly under the sky bridge
+
+// This is an example of creating an autonomous mode based off of
+// BaseAutonomous (we 'extend' it)
+
+@Autonomous(name="Autonomous3")
+public class Autonomus3 extends BaseAutonomous {
+
+    // The BaseAutonomous class will have it's runOpsMode function called.  It will set up
+    // basic stuff for us, but then it calls it's own runAutoOpMode, which does nothing.
+    // We 'overrid' runAutoOpMode to do *something*
+
+    @Override
+    public void runAutoOpMode() {
+
+
+        driveStraight(0.5, 24.0, 15.0);
+        spinRight(-90,0.5);
+        driveStraight(0.5,15,15);
+    }
+}
