c620e0ba69449760d5d9476c4bb09b45aaf9f35b
Paul Marganian <pmargani@nrao.edu>
2020-01-09 20:00:06 -0500

    changed name, and using spin proportional

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous2.java
@@ -14,7 +14,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 // This is an example of creating an autonomous mode based off of
 // BaseAutonomous (we 'extend' it)
 
-@Autonomous(name="AutonomousBlue")
+@Autonomous(name="AutonomousLeft2")
 public class Autonomous2 extends BaseAutonomous {
 
     // The BaseAutonomous class will have it's runOpsMode function called.  It will set up
@@ -28,7 +28,7 @@ public class Autonomous2 extends BaseAutonomous {
         sleep(2000);
 
         driveStraight(0.5, 24.0, 15.0);
-        spinLeft(70,0.5);
+        spinLeftP(90,0.5);
         driveStraight(0.5,20,15);
     }
 }
